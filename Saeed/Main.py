d9ec21ae588b91794fa1787d93e94fbacc29b21a
Saeed Ahmed <saeedahmedbl68@gmail.com>
2026-05-11 23:42:35 +0500

    Create Main.py

---
+++ Saeed/Main.py
@@ -0,0 +1,11 @@
+name = "Saeed"
+Age = 25
+City = "Taunsa Sharif"
+University = "University of the Punjab, Lahore"
+
+print("Personal Information:")
+print("---------------------")
+print("Name:", name)
+print("Age:", Age)
+print("City:", City)
+print("University:", University)   
